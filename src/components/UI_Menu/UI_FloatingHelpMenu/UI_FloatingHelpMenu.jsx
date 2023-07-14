f23c16248dc0cbd647aefeb70845406fe3d88950
Innis <1575270674@qq.com>
2023-07-14 11:29:54 +0800

    feat: connect to  gitHub issues

--- src/components/UI_Menu/UI_FloatingHelpMenu/UI_FloatingHelpMenu.jsx
+++ src/components/UI_Menu/UI_FloatingHelpMenu/UI_FloatingHelpMenu.jsx
@@ -3,14 +3,33 @@ import { Menu, Trigger } from '@arco-design/web-react';
 import {IconMessage, IconClose, IconBug, IconBulb, IconQuestion, IconBook, IconFile} from '@arco-design/web-react/icon';
 const MenuItem = Menu.Item;
 import "./UI_FloatingHelpMenu.css"
+import {useNavigate} from "react-router-dom";
+import {sendAIssue} from "../../../tools/github.js";
 
 export default function UI_FloatingHelpMenu() {
+    const navigate = useNavigate();
+    function onClickMenuItem(key) {
+        if(key === "1"){
+            console.log("bug");
+        }else if(key === "2"){
+            console.log("idea");
+            sendAIssue().then((res) => {
+                console.log(res);
+            });
+        }else if(key === "3"){
+            window.open("https://drive.google.com/drive/folders/14sulRff83Fq2i_GnP1kGPZ3DLDyZSyb2?usp=sharing", "_blank");
+        }
+
+    }
+
+
     const renderMenu = () => {
         return (
             <Menu
                 mode='popButton'
                 tooltipProps={{ position: 'left' }}
                 hasCollapseButton
+                onClickMenuItem={onClickMenuItem}
             >
                 <MenuItem key='1'>
                     <IconBug />
