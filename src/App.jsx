c8ca410ca446b1d51fb1e1b2764d24639c99a9e1
Yannis <lengzhiyan1015@gmail.com>
2023-07-19 20:34:50 +0800

    to: settings

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Frame from "./pages/Frame/Frame.jsx";
 import Login from "./pages/Login/Login.jsx";
 import "./App.css";
@@ -12,7 +12,8 @@ import Interview_form from "./pages/Recruitment/InterviewPage/Interview_form.jsx
 import Recruitment_Evaluation_Table from "./pages/Recruitment/EvaluationPage/EvaluationTable.jsx";
 import Evaluation_Page from "./pages/Recruitment/EvaluationPage/EvaluationPage.jsx";
 import Registration from './pages/Registration/Registration.jsx';
-import SettingsPage from "./pages/Settings/SettingsPage.jsx";
+import SettingsPage, { Calendar, About } from './pages/Settings/SettingsPage/SettingsPage.jsx';
+import Account from './pages/Settings/Account/Account.jsx';
 
 function App() {
 
@@ -20,25 +21,30 @@ function App() {
 
     return (
         <>
-             <Router>
-                    <Routes>
-                        <Route path="/login" element={<Login/>} />
-                        <Route path="/" element={<Frame/>} >
-                            <Route path="/recruitment_dashboard" element={<Recruitment_Dashboard/>} />
-                            <Route path="/recruitment_add_candidate" element={<Recruitment_Submission/>} />
-                            <Route path="/recruitment_pre_screening" element={<PreScreening_table/>} />
-                            <Route path="/recruitment_pre_screening/:RID" element={<PreScreening/>} />
-                            <Route path="/recruitment_interview" element={<Interview_table/>} />
-                            <Route path="/recruitment_interview/form/:RID/:partID" element={<Interview_form/>} />
-                            <Route path="/recruitment_evaluation" element={<Recruitment_Evaluation_Table/>} />
-                            <Route path="/recruitment_evaluation/form/:RID" element={<Evaluation_Page/>} />
-                        </Route>
-                        <Route path="/interviewer" element={<Registration/>} />
-                        <Route path="/profile" element={<Profile/>} />
-                        <Route path="/settings" element={<SettingsPage/>} />
-                        <Route path="/testing" element={<Testing/>} />
-                        <Route path="*" element={<Login/>} />
-                    </Routes>
+            <Router>
+                <Routes>
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/" element={<Frame />} >
+                        <Route path="/recruitment_dashboard" element={<Recruitment_Dashboard />} />
+                        <Route path="/recruitment_add_candidate" element={<Recruitment_Submission />} />
+                        <Route path="/recruitment_pre_screening" element={<PreScreening_table />} />
+                        <Route path="/recruitment_pre_screening/:RID" element={<PreScreening />} />
+                        <Route path="/recruitment_interview" element={<Interview_table />} />
+                        <Route path="/recruitment_interview/form/:RID/:partID" element={<Interview_form />} />
+                        <Route path="/recruitment_evaluation" element={<Recruitment_Evaluation_Table />} />
+                        <Route path="/recruitment_evaluation/form/:RID" element={<Evaluation_Page />} />
+                    </Route>
+                    <Route path="/interviewer" element={<Registration />} />
+                    <Route path="/profile" element={<Profile />} />
+                    <Route path="/settings" element={<SettingsPage />} >
+                        <Route path="/settings/account" element={<Account />} />
+                        <Route path="/settings/calendar" element={<Calendar />} />
+                        <Route path="/settings/about" element={<About />} />
+                        <Route path="/settings" element={<Navigate to="/settings/account" />} />
+                    </Route>
+                    {/* <Route path="/testing" element={<Testing/>} /> */}
+                    <Route path="*" element={<Login />} />
+                </Routes>
             </Router>
         </>
 
