9ddd97959eae85e5e04fa17bc8777cb2c0125f4e
ETCasual <casualorient@gmail.com>
2023-08-08 12:18:50 +0800

    fix env

--- src/config.js
+++ src/config.js
@@ -1,4 +1,1 @@
-export const hostURL =
-	import.meta.env['MODE'] === 'development'
-		? import.meta.env['VITE_CLOUD_HOST_URL']
-		: import.meta.env['VITE_PROD_HOST_URL'];
+export const hostURL = import.meta.env['VITE_HOST_URL'];
