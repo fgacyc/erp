66d6c6c88bdb20c8f59eeabe8b0e294d99d63782
Innis <1575270674@qq.com>
2023-09-19 13:40:52 +0800

    feat:add form.List

--- src/components/UI_Modal/UI_Education/AddCoursesModal.tsx
+++ src/components/UI_Modal/UI_Education/AddCoursesModal.tsx
@@ -1,8 +1,10 @@
-import {Form, FormInstance, Input, Modal} from "@arco-design/web-react";
+import {Button, Form, FormInstance, Input, Modal, Space} from "@arco-design/web-react";
 const FormItem = Form.Item;
 const TextArea = Input.TextArea;
+import "./style.css";
 
 import {FunctionComponent, useRef} from "react";
+import {IconDelete} from "@arco-design/web-react/icon";
 
 interface AddCoursesModalProps {
     visible: boolean;
@@ -37,6 +39,53 @@ export const AddCoursesModal: FunctionComponent<AddCoursesModalProps> = ({visibl
                     <TextArea placeholder="please enter Credit's description..."
                               className={"w-[300px] resize-none"}/>
                 </FormItem>
+                <Form.List field='users'>
+                    {(fields, { add, remove }) => {
+                        return (
+                            <div>
+                                {fields.map((item, index) => {
+                                    return (
+                                        <div key={item.key}>
+                                            <Form.Item label={"User " + index}>
+                                                <Space>
+                                                    <Form.Item
+                                                        field={item.field + ".username"}
+                                                        rules={[{ required: true }]}
+                                                        noStyle
+                                                    >
+                                                        <Input />
+                                                    </Form.Item>
+                                                    <Form.Item
+                                                        field={item.field + ".address"}
+                                                        rules={[{ required: true }]}
+                                                        noStyle
+                                                    >
+                                                        <Input />
+                                                    </Form.Item>
+                                                    <Button
+                                                        icon={<IconDelete />}
+                                                        shape='circle'
+                                                        status='danger'
+                                                        onClick={() => remove(index)}
+                                                    ></Button>
+                                                </Space>
+                                            </Form.Item>
+                                        </div>
+                                    );
+                                })}
+                                <Form.Item wrapperCol={{ offset: 5 }}>
+                                    <Button
+                                        onClick={() => {
+                                            add();
+                                        }}
+                                    >
+                                        Add User
+                                    </Button>
+                                </Form.Item>
+                            </div>
+                        );
+                    }}
+                </Form.List>
             </Form>
         </Modal>
     );
