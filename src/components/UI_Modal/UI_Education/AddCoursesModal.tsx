09245ea6918c32d6572f3ac23c434d310fe44809
Innis <1575270674@qq.com>
2023-09-19 13:34:01 +0800

    fix: add course form

--- src/components/UI_Modal/UI_Education/AddCoursesModal.tsx
+++ src/components/UI_Modal/UI_Education/AddCoursesModal.tsx
@@ -1,11 +1,17 @@
-import { Modal } from "@arco-design/web-react";
-import {FunctionComponent} from "react";
+import {Form, FormInstance, Input, Modal} from "@arco-design/web-react";
+const FormItem = Form.Item;
+const TextArea = Input.TextArea;
+
+import {FunctionComponent, useRef} from "react";
 
 interface AddCoursesModalProps {
     visible: boolean;
     setVisible: (visible: boolean) => void;
 }
 export const AddCoursesModal: FunctionComponent<AddCoursesModalProps> = ({visible, setVisible}) => {
+    const formRef = useRef<FormInstance | null>(null);
+    const [form] = Form.useForm();
+
     return (
         <Modal
             title="Add Courses"
@@ -15,10 +21,23 @@ export const AddCoursesModal: FunctionComponent<AddCoursesModalProps> = ({visibl
             autoFocus={false}
             focusLock={true}
         >
-            <p>
-                You can customize modal body text by the current situation. This modal
-                will be closed immediately once you press the OK button.
-            </p>
+            <Form autoComplete='off'
+                  layout="horizontal"
+                  ref={formRef}
+                  form={form}
+                  className={"modal-form-style-fix"}>
+                <FormItem label={"Name"} field="name" className={"flex-nowrap"} rules={[{
+                    required: true, message: "Please select genre"
+                }]}>
+                    <Input placeholder="please enter Credit's Chinese name..." className={"w-[300px]"}/>
+                </FormItem>
+                <FormItem label='Description' field="description" className={"flex-nowrap"} rules={[{
+                    required: true, message: "Please select genre"
+                }]}>
+                    <TextArea placeholder="please enter Credit's description..."
+                              className={"w-[300px] resize-none"}/>
+                </FormItem>
+            </Form>
         </Modal>
     );
 };
